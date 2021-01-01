74ca8a48d0352cc366caece5960717637d49fcac
unknown <>
2021-01-01 23:13:35 +0100

    update gui-menu-example.py

--- gui-menu-example.py
+++ gui-menu-example.py
@@ -5,9 +5,17 @@ from tkinter import Menu
 
 win = tk.Tk(className="Python Gui with menu")
 win.geometry("300x300")
+
 #adding modified label
 m_label = ttk.Label(win, text="Modified label")
 m_label.grid(column=1, row=1)
+
+#exit functions
+def _quit():
+    win.quit()
+    win.destroy()
+    exit()
+
 #create menu bar
 menuBar = Menu(win)
 win.config(menu=menuBar)
@@ -16,8 +24,13 @@ win.config(menu=menuBar)
 file_menu = Menu(menuBar, tearoff=0) #create File menuBar
 file_menu.add_command(label="New") #add File menu item
 file_menu.add_separator() #add separator
-file_menu.add_command(label="Exit") #add next File menu item
+file_menu.add_command(label="Exit", command=_quit) #add next File menu item
 menuBar.add_cascade(label="File", menu=file_menu)
+
+#add another menu with items
+help_menu = Menu(menuBar, tearoff=0)
+menuBar.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="About")
 
 #gui call
 win.mainloop()
