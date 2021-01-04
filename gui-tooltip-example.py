059a5ce81f5ccd4b04fb15b9c26bd9507409e568
unknown <>
2021-01-04 14:04:09 +0100

    def class tooltip

---
+++ gui-tooltip-example.py
@@ -0,0 +1,31 @@
+#import or functions from tkinter library
+import tkinter as tk
+from tkinter import ttk
+from tkinter import Menu
+from tkinter import messagebox as msg
+
+#define tooltip class
+class Tooltip(object):
+    def __init__(self, widget, tip_text=None):
+        self.widget = widget
+        self.tip_text = tip_text
+        widget.bind('<Enter>', self.mouse_enter)
+        widget.bind('<Leave', self.mouse_leave)
+
+    def mouse_enter(self, _event):
+        self.show_tooltip()
+
+    def mouse_leave(self, _event):
+        self.hide_tooltip()
+
+    def show_tooltip(self):
+        if self.tip_window:
+            x_left = self.widget.winfo_rootx()
+            y_top = self.widget.winfo_rooty() - 18
+            pass
+
+    def hide_tooptip(self):
+        pass
+
+#end define class
+ 
